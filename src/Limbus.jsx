4bc9ba4050af40a01f112bdfd62da44ffadcdd29
Takumi Hirabayashi <221205139@ccmailg.meijo-u.ac.jp>
2024-03-04 23:21:49 +0900

    コインをコンポーネントに分割

--- src/Limbus.jsx
+++ src/Limbus.jsx
@@ -1,13 +1,19 @@
 import './Limbus.css';
 
+const Coin = (props) => {
+  return (
+    <div className='coin'>
+      <img src='images/coin.png' className='coinImage'/>
+      <p>{props.coinPower}</p>
+    </div>
+  );
+}
+
 function Limbus() {
   return (
     <div className='Limbus'>
       <p>Limbus</p>
-      <div className='coinField'>
-        <img src='images/coin.png' className='coin'/>
-        <p>1</p>
-      </div>
+      <Coin coinPower="1"/>
       <button className='simulationButton'>決定</button>
     </div>
   );
